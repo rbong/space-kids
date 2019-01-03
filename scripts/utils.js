07c1c17aedace6af2fe8b64ea9a519d695e33006
rbong <r.l.bongers@gmail.com>
2019-01-02 19:43:00 -0500

    Fix byte math

--- scripts/utils.js
+++ scripts/utils.js
@@ -12,29 +12,29 @@ function writeDataFile(file, data) {
 }
 
 function sliceBytes(data, nBytes = 1, join = false) {
-  let bytes = data.slice(Number(nBytes) * 2);
+  let bytes = data.slice(Number(nBytes));
   return join ? bytes.join('') : bytes;
 }
 
 function shiftBytes(data, nBytes = 1, join = false) {
-  let bytes = data.splice(0, Number(nBytes) * 2);
+  let bytes = data.splice(0, Number(nBytes));
   return join ? bytes.join('') : bytes;
 }
 
-function shiftU8(data) {
-  return Buffer.from(shiftBytes(data, 1, true), 'hex').readUInt8();
-}
-
 function shiftU16(data) {
-  return Buffer.from(shiftBytes(data, 2, true), 'hex').readUInt16LE();
-}
-
-function shiftU24(data) {
-  return Buffer.from(shiftBytes(data, 3, true) + '00', 'hex').readUInt32LE();
+  return Buffer.from(shiftBytes(data, 2, true), 'hex').readUInt8();
 }
 
 function shiftU32(data) {
-  return Buffer.from(shiftBytes(data, 4, true), 'hex').readUInt32LE();
+  return Buffer.from(shiftBytes(data, 4, true), 'hex').readUInt16LE();
+}
+
+function shiftU48(data) {
+  return Buffer.from(shiftBytes(data, 6, true) + '00', 'hex').readUInt32LE();
+}
+
+function shiftU64(data) {
+  return Buffer.from(shiftBytes(data, 8, true), 'hex').readUInt32LE();
 }
 
 function splitDataFile(file, getName) {
@@ -42,18 +42,18 @@ function splitDataFile(file, getName) {
   const originalData = readDataFile(file);
   let data = Array.from(originalData);
 
-  const fatSize = shiftU16(data);
+  const fatSize = shiftU32(data);
   const nFiles = fatSize / 4;
   const fat = [];
   for (let i = 0; i < nFiles; i++) {
-    fat[i] = shiftU24(data);
+    fat[i] = shiftU48(data);
     // Unknown value
-    shiftU8(data);
+    shiftU16(data);
   }
 
   for (let i = 0; i < nFiles; i++) {
     data = sliceBytes(originalData, fat[i]);
-    const size = shiftU16(data);
+    const size = shiftU32(data);
     const chunk = shiftBytes(data, size);
     console.log('read', size, 'bytes starting from', '0x' + fat[i].toString(16));
     const name = getName(i);
@@ -76,10 +76,10 @@ module.exports = {
   writeDataFile,
   sliceBytes,
   shiftBytes,
-  shiftU8,
   shiftU16,
-  shiftU24,
   shiftU32,
+  shiftU48,
+  shiftU64,
   splitDataFile,
   sh,
   mkdir
